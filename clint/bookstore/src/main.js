120a7f24ad4d98ac208442bc70731c52ce2a4629
ljmljm12345 <1394955202@qq.com>
2020-09-22 09:38:33 +0800

    123

--- clint/bookstore/src/main.js
+++ clint/bookstore/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
+Vue.use(Element);
 
 axios.defaults.baseURL="http://192.168.3.124:7001"
 
